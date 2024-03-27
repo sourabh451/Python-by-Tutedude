4bc2450f6f8c5e79e25c0737702a6ee343c96c93
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 10:31:50 +0530

    Module 3 : Control Structures In Python 4. Range function

---
+++ 2pythonProject/3.4range.py
@@ -0,0 +1,22 @@
+#range
+
+'''
+n=list(range(10))
+print(n)
+'''
+
+'''
+n=list(range(2,11))
+print(n)
+
+#start value:2
+#end value:11
+#step:
+'''
+
+n=list(range(2,11,2))
+print(n)
+
+#start value:2
+#end value:11
+#step:2
