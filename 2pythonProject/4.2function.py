8a9fbf38eb0e962212f148f50a98a29c1256ba59
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 19:27:50 +0530

    Module 4 : Functions & Modules In Python 2. Return statement

---
+++ 2pythonProject/4.2function.py
@@ -0,0 +1,24 @@
+#def func():
+#   statements
+#   ...
+#   return (expr)
+
+#function call
+
+def add(a,b):
+    print(a+b)
+add(1,2)
+
+def add1(a,b):
+    c = a + b
+    return c
+
+ans = add1(10,20)
+print(ans)
+
+def mult(a,b):
+    c = a * b
+    return c
+
+result = mult(2, 'Mike')
+print(result)
