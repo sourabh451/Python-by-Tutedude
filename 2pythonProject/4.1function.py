dbef501900b74ad9d9e570fd7ba5732aaa0dd971
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 19:14:05 +0530

    Module 4 : Functions & Modules In Python 1. Code reusing using functions

---
+++ 2pythonProject/4.1function.py
@@ -0,0 +1,22 @@
+#code_reusing
+
+#def "function_name"():
+    #statements
+
+#fuction call
+
+'''
+def str1():
+    print('basketball')
+    print('cricket')
+
+str1()
+str1()
+'''
+
+def str_add():
+    str1= "hello"
+    str2= " world"
+    print(str1 + str2)
+
+str_add()
