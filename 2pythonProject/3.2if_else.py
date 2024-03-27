7a2fa2f6fff2ba844a89ae36ffa309dfac8f76ab
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 09:47:26 +0530

    Module 3 : Control Structures In Python 2. If else

---
+++ 2pythonProject/3.2if_else.py
@@ -0,0 +1,24 @@
+'''
+marks=int(input('Enter your marks: '))
+
+#if (test expression):
+    #statement
+#else:
+    #statement
+#statement
+
+
+if (marks>=40):
+    print("Passed")
+else:
+    print("Failed")
+print("Thank you")
+'''
+
+age=int(input('Enter the age: '))
+
+if age>18:
+    print("You can vote.")
+else:
+    print("You cannot vote")
+
