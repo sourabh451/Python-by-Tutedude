b8a2676f5170d7d6616503f9c2ca92f3d2b75645
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 12:13:06 +0530

    Module 3 : Control Structures In Python 7. While loop

---
+++ 2pythonProject/3.7while.py
@@ -0,0 +1,30 @@
+#'a' ---> 5 times
+
+#counter variable
+#while (test expr.):
+    #statement
+    #increment / decrement
+'''
+counter = 1
+while counter <= 5:
+    print('a')
+    counter = counter + 1
+'''
+'''
+counter = 5
+while counter >= 1:
+    print('apple')
+    counter = counter - 1
+'''
+
+'''
+count = 0
+while (count<=10):
+    print(count)
+    count = count + 1
+'''
+
+counter=1
+while (counter <= 5):
+    print("Python")
+    counter = counter + 1
