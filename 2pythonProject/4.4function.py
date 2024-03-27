52361725382b07a429b03d1a3592180851d2d885
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 19:45:16 +0530

    Module 4 : Functions & Modules In Python 4. Passing functions as arguments

---
+++ 2pythonProject/4.4function.py
@@ -0,0 +1,11 @@
+# 2 numbers, if you add them=7
+# square of the number
+# 1 function call to be used
+
+def add(x,y):
+    return x + y
+def square(z):
+    return z * z
+
+result= square(add(6,1))
+print(result)
