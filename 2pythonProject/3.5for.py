bc131d82dd464f511e34983b90f88548cec02dc8
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-27 11:36:46 +0530

    Module 3 : Control Structures In Python 5. For loop

---
+++ 2pythonProject/3.5for.py
@@ -0,0 +1,25 @@
+'''
+l=['Mike',10.2,1998]
+for i in l:
+    print(i)
+'''
+
+'''
+for i in ['Mike',10.2,1998]:
+    print(i)
+'''
+
+'''
+x='apple'
+for i in x:
+    print(i)
+'''
+
+'''
+for i in range(1,11):
+    print(i)
+'''
+
+#a --> 10 times
+for i in range(1,11):
+    print('a')
