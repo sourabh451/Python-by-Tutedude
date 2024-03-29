7a40c2d7c839046c73485c9aef79a8561c61c35d
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-29 12:10:35 +0530

    Module 6 : DATA STRUCTURES AND STRINGS 4. List Operations

---
+++ 2pythonProject/6.4list.py
@@ -0,0 +1,20 @@
+'''
+list1=['Mike',10.1,1998]
+list2=['pop',1]
+print(list1 + list2)
+'''
+
+'''
+a=['disco',10,1.2]
+a[0]='hardwork'
+print(a)
+'''
+
+
+# LISTS ARE MUTABLE
+n=[1,2,3]
+print(n * 3)
+
+game=['football','basketball','chess']
+print('football' in game)
+print('cricket' in game)
