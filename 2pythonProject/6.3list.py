c690587f9f0dacba6c7e0c0b62280be9d56c1686
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-29 11:15:35 +0530

    Module 6 : DATA STRUCTURES AND STRINGS 3. List 1

---
+++ 2pythonProject/6.3list.py
@@ -0,0 +1,25 @@
+# List
+
+# EG: Writing a list for the groceries
+'''
+a=1
+b=2
+c=3
+d=4
+e=5
+print(c)
+'''
+
+number=[1,2,3,4,5]
+#       0 1 2 3 4
+print(number[2])
+
+colors=['black','red','white','blue']
+#           0     1      2       3
+print(colors[1])
+
+mix=['Peter',10,'Sam',15]
+#       0    1    2    3
+print(mix[2])
+
+blank_list=[]
