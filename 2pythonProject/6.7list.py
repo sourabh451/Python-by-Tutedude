da83b12a5160b728d235fcc043408e52dbf55140
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-29 13:53:08 +0530

    Module 6 : DATA STRUCTURES AND STRINGS 7. List slicing

---
+++ 2pythonProject/6.7list.py
@@ -0,0 +1,9 @@
+numbers= [10,20,30,40,50,60,70]
+#         0  1  2  3  4  5  6
+# numbers[n:n+1:interval]
+
+print(numbers[0:5])
+print(numbers[2:6])
+print(numbers[:4])
+print(numbers[3:])
+print(numbers[0:5:2])
