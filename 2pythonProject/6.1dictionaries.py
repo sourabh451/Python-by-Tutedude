b062e982347d683da1cc96fe65c740d95a35de19
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-03-29 09:54:19 +0530

    Module 6 : DATA STRUCTURES AND STRINGS 1. Dictionaries

---
+++ 2pythonProject/6.1dictionaries.py
@@ -0,0 +1,13 @@
+# list ----> [] , To specify element = index value
+# list = [1,2,3,4,5]
+
+# dictionary ----> {} , To specify element = Key
+# dictionary = {'key1':1,'key2':'2','key3':[333],'key4':(444),'key5':5}
+
+my_dict = {'key1':1,'key2':'2','key3':[333],'key4':(444),'key5':5}
+print(my_dict)
+print(my_dict['key3'])
+
+dictionary = {1:'a',2:'b',3:'c'}
+print(dictionary)
+print(dictionary[2])
