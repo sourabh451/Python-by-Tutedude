d1f400272ca1f0ac710cd5bfd514a359e6149fb7
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-04 16:10:28 +0530

    Module 8 : OBJECT ORIENTED PROGRAMMING 1. Introduction to object oriented programming

---
+++ 2pythonProject/8.1oop.py
@@ -0,0 +1,6 @@
+# object oriented programming
+
+#class - blue print for creating objects
+# objects - instance, created as many times as you want
+# attributes - characteristics associated to a type(obj.)
+# methods - functions associated to a type
