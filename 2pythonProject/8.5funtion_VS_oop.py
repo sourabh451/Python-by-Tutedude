4cf251e17696d132c93bbe722a52440136318e63
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-04 17:13:55 +0530

    Module 8 : OBJECT ORIENTED PROGRAMMING 5. Functions vs object oriented programming

---
+++ 2pythonProject/8.5funtion_VS_oop.py
@@ -0,0 +1,17 @@
+'''
+def name(name):
+    print("Hi", name)
+
+n = name("Jim")
+n = name("Sam")
+'''
+
+class name:
+    x = 0
+    name = ""
+    def __init__(self,z):
+        self.name = z
+        print("Hi",z)
+
+n = name("Jim")
+n = name("Sam")
