b75ce9f7277168c1c517a74f49d7c0304b65bf98
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-11 20:51:43 +0530

    Module 10 : TKINTER 2. Basic modifications using tkinter

---
+++ 2pythonProject/10.2BasicModificationsUsingTkinter.py
@@ -0,0 +1,13 @@
+# step1: import tkinter
+from tkinter import *
+
+# step2: gui interaction
+window = Tk()
+
+# step3: adding inputs
+window.title("Simple")
+window.geometry("500x700")
+window.config(bg = "yellow")
+
+# step4: main loop
+mainloop()
