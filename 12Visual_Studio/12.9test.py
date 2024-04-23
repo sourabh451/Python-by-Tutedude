2be6ae8b16e50db92ad5e92a0144224506b9f219
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-23 08:27:06 +0530

    Module 12. Building Database Apps with PostgreSQL & Python 13. Inserting the data using python

--- 12Visual_Studio/12.9test.py
+++ 12Visual_Studio/12.9test.py
@@ -1,10 +1,22 @@
 import psycopg2
-conn = psycopg2.connect(dbname="postgres",user="postgres",password="root",host="localhost",port="5432")
+def table():
+    conn = psycopg2.connect(dbname="postgres",user="postgres",password="root",host="localhost",port="5432")
 
-cursor = conn.cursor()
-cursor.execute('''create table employees(Name Text, ID int, Age int);''')
-print('Table created successfully')
+    cursor = conn.cursor()
+    cursor.execute('''create table employees(Name Text, ID int, Age int);''')
+    print('Table created successfully')
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
 
+def data():
+    conn = psycopg2.connect(dbname="postgres",user="postgres",password="root",host="localhost",port="5432")
+
+    cursor = conn.cursor()
+    cursor.execute('''insert into employees(Name,ID,Age) values('Sam', 01, 30);''')
+    print('Data added successfully')
+
+    conn.commit()
+    conn.close()
+
+data()
