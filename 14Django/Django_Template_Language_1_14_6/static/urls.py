11d188a45280669d8d8e62ccf203f6dca83194c1
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-26 15:19:16 +0530

    Module 14 : DJANGO 11. Static files 1

--- 14Django/Django_Template_Language_1_14_6/static/urls.py
+++ 14Django/Django_Template_Language_1_14_6/static/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from Design import views
+# from django.contrib import admin
+from django.urls import path
+from static import views
 # from Design.views import hello
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('static.urls'))
+    path('', views.index,name="index"),
 ]
