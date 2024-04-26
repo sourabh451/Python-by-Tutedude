f0c32f0b1cd33e4de037b291c5420c402e79828d
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-26 11:29:12 +0530

    Module 14 : DJANGO 8. Adding numbers

--- 14Django/Django_Template_Language_1_14_6/Design/views.py
+++ 14Django/Django_Template_Language_1_14_6/Design/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 def hello(request):
     #return HttpResponse("<h1> Welcome </h1>")
-    return render(request,'home.html', {'name':'Python'})
+    return render(request,'home.html', {'name':'Tom'})
+
+def add(request):
+    value_1 = int(request.GET['number 1'])
+    value_2 = int(request.GET['number 2'])
+    res = value_1+value_2
+    return render(request,'result.html',{'result':res})
