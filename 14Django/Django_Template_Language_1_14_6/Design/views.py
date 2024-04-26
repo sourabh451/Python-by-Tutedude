afa3029784e43f2853b0a413cb3d9f0c0cc032aa
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-26 14:30:47 +0530

    Module 14 : DJANGO 9. GET vs POST

--- 14Django/Django_Template_Language_1_14_6/Design/views.py
+++ 14Django/Django_Template_Language_1_14_6/Design/views.py
@@ -6,7 +6,7 @@ def hello(request):
     return render(request,'home.html', {'name':'Tom'})
 
 def add(request):
-    value_1 = int(request.GET['number 1'])
-    value_2 = int(request.GET['number 2'])
+    value_1 = int(request.POST['number 1'])
+    value_2 = int(request.POST['number 2'])
     res = value_1+value_2
     return render(request,'result.html',{'result':res})
