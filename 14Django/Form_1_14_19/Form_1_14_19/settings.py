539a98e77d805bedfe9ef95fb2e4a4719474eff9
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-27 15:23:02 +0530

    Module 14 : DJANGO 21. Styling forms

--- 14Django/Form_1_14_19/Form_1_14_19/settings.py
+++ 14Django/Form_1_14_19/Form_1_14_19/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'crispy_forms', 'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
