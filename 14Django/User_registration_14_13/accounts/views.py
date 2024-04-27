634d6e3f036d345057c266f498f2487069cc6ef4
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-27 08:21:08 +0530

    Module 14 : DJANGO 15. User registration(part 3)

--- 14Django/User_registration_14_13/accounts/views.py
+++ 14Django/User_registration_14_13/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def register(request):
+    if request.method=="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request,"registration/register.html", {'form':form})
