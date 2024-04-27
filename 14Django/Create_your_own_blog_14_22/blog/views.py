840ed6cd809309238c2f540ebf378fa8b3dd2138
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-27 17:01:04 +0530

    Module 14 : DJANGO 24. Create your own blog 3

--- 14Django/Create_your_own_blog_14_22/blog/views.py
+++ 14Django/Create_your_own_blog_14_22/blog/views.py
@@ -4,8 +4,8 @@ from blog.models import Post
 # Create your views here.
 def allpost(request):
     post = Post.objects
-    return render(request,'post.html',{'post':post})
+    return render(request,'posts.html',{'post':post})
 
 def detail(request, blog_id):
-    detail = get_object_or_404('Post.pk = blog_id')
-    return render(request,'detail.html',{'post':detail})
+    detail = get_object_or_404(Post,pk = blog_id)
+    return render(request,'details.html',{'post':detail})
