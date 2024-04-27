340ea1790c860a8ad5693de4c22aeacb8c4a3eba
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-27 08:01:33 +0530

    Module 14 : DJANGO 14. User registration(part 2)

--- 14Django/User_registration_14_13/accounts/urls.py
+++ 14Django/User_registration_14_13/accounts/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from accounts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("accounts.urls"))
+    path('', views.index,name="index"),
+    path('register', views.register,name="register"),
+
 ]
