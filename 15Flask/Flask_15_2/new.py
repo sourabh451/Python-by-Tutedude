f24169fa2b2929edef9d12b9c3e34ad1bf8a3d22
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-29 07:31:57 +0530

    Module 15 : FLASK 4. Basic webpage 2

--- 15Flask/Flask_15_2/new.py
+++ 15Flask/Flask_15_2/new.py
@@ -1,13 +1,17 @@
+# IMPORTING
 from flask import Flask
 
+# INTERACTION
 web = Flask(__name__)
 
+# MAPPING
 @web.route('/')
+@web.route('/home')
 
+# INPUTS
 def home():
     return "Welcome"
 
+# MAIN
 if __name__ == "__main__":
     web.run(debug=True)
-
-# www.youtube.com
