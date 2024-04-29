2900f96bc4260d005a9868e0429d027999d6f859
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-29 07:24:20 +0530

    Module 15 : FLASK 3. Basic webpage 1

--- 15Flask/Flask_15_2/new.py
+++ 15Flask/Flask_15_2/new.py
@@ -1,1 +1,13 @@
-print("Hello")
+from flask import Flask
+
+web = Flask(__name__)
+
+@web.route('/')
+
+def home():
+    return "Welcome"
+
+if __name__ == "__main__":
+    web.run(debug=True)
+
+# www.youtube.com
