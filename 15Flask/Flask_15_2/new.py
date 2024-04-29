3bfce32b934e11fb0309591ae889b8211a76b3c5
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-29 08:06:35 +0530

    Module 15 : FLASK 7. Basic Registration form 2

--- 15Flask/Flask_15_2/new.py
+++ 15Flask/Flask_15_2/new.py
@@ -1,5 +1,5 @@
 # IMPORTING
-from flask import Flask , render_template
+from flask import Flask, render_template, request
 
 # INTERACTION
 web = Flask(__name__)
@@ -11,6 +11,16 @@ web = Flask(__name__)
 # INPUTS
 def homepage():
     return render_template('register.html')
+
+# MAPPING
+@web.route("/confirmation", methods=['POST','GET'])
+# INPUT
+def register():
+    if request.method == "POST":
+        n = request.form.get('name')
+        c = request.form.get('city')
+        p = request.form.get('phone number')
+        return render_template('confirm.html', name=n, city=c, phonenumber=p)
 
 # MAIN
 if __name__ == "__main__":
