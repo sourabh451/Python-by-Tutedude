743ed88d1e13afe091b9a9eabb28ce40af84ecd8
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-30 13:04:46 +0530

    Module 16 : REST API's USING DJANGO 7. Adding username to the posts

--- 16REST_API's_USING_DJANGO/blog_16_2/helloworld/models.py
+++ 16REST_API's_USING_DJANGO/blog_16_2/helloworld/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -6,6 +7,6 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
