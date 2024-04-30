96f5ac154638c785ad004e29c4dcfb253ac82ecf
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-04-30 10:06:01 +0530

    Module 16 : REST API's USING DJANGO 5. Segregating into pages

--- 16REST_API's_USING_DJANGO/blog_16_2/blog_16_2/settings.py
+++ 16REST_API's_USING_DJANGO/blog_16_2/blog_16_2/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
