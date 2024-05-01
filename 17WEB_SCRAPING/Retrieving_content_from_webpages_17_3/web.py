7e41afa90d478e0fad6d49821b2abf7554bd1a7d
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-01 13:39:50 +0530

    Module 17 : WEB SCRAPING 6. Content

--- 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
@@ -23,6 +23,7 @@ print(response.headers)
 print(response.request.headers)
 """
 
+"""
 import requests
 url = "http://127.0.0.1:8000/helloworld/"
 user = {
@@ -30,3 +31,20 @@ user = {
 }
 response = requests.get(url = url, headers=user)
 print(response.request.headers)
+"""
+
+
+import requests
+#url = "http://127.0.0.1:8000/helloworld/"
+url = "http://127.0.0.1:8000/picture/images/nature-images.jpg"
+user = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+}
+response = requests.get(url = url, headers=user)
+#print(response.content)
+#print(type(response.content))
+#print(response.content)
+pic = response.content
+
+f = open("nature-images.jpg","wb") #wb =writing byte
+f.write(pic)
