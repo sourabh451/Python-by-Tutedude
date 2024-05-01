706f1c95d5bde2e1394bf55c79135192427ef5bc
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-01 15:05:22 +0530

    Module 17 : WEB SCRAPING 10. Filtering the data 1

---
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web_5.py
@@ -0,0 +1,13 @@
+# pip install bs4 #helps us to get only necessary data out of the html and xml files
+# pip install lxml # helps in of easy handling of xml and html file
+
+import requests
+from bs4 import BeautifulSoup
+def Extract(url):
+    response = requests.get(url=url).content
+    soup = BeautifulSoup(response, 'lxml')
+    #tag = soup.div
+    tag = soup.find('div' , {"id" : "mp-right"})
+    print(tag)
+
+Extract(url="https://en.wikipedia.org/wiki/Main_Page")
