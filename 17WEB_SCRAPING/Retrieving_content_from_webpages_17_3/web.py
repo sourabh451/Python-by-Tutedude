60c17343b393633ac7768cdd800b80e55adb0b05
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-01 13:29:30 +0530

    Module 17 : WEB SCRAPING 5. Stating the data using dictionaries

--- 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
@@ -8,8 +8,8 @@ for line in url:
     print(line.decode().strip())
 """
 
+"""
 # pip install requests
-
 import requests
 
 url = "http://127.0.0.1:8000/helloworld/"
@@ -20,4 +20,13 @@ print(response)
 print(dir(response))
 print(response.status_code)
 print(response.headers)
+print(response.request.headers)
+"""
+
+import requests
+url = "http://127.0.0.1:8000/helloworld/"
+user = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+}
+response = requests.get(url = url, headers=user)
 print(response.request.headers)
