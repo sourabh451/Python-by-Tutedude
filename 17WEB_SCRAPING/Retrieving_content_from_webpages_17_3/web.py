5ecc3a54963d375540e72d77a9c35390dec128d4
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-01 13:22:16 +0530

    Module 17 : WEB SCRAPING 4. Getting the data

--- 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web.py
@@ -1,6 +1,23 @@
+# pip install urllib3
+"""
 import urllib.request, urllib.parse, urllib.error
 
 url = urllib.request.urlopen("http://127.0.0.1:8000/helloworld/")
 
 for line in url:
     print(line.decode().strip())
+"""
+
+# pip install requests
+
+import requests
+
+url = "http://127.0.0.1:8000/helloworld/"
+
+#print(dir(requests))
+response = requests.get(url = url)
+print(response)
+print(dir(response))
+print(response.status_code)
+print(response.headers)
+print(response.request.headers)
