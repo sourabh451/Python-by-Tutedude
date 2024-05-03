0130f36dcddb3021b0719f8dba35a0a82a6e9537
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-03 14:03:25 +0530

    Module 17 : WEB SCRAPING 16. Downloading images 2

--- 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web_6.py
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web_6.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 user = input("Enter the image name: ")
 
@@ -7,6 +8,19 @@ user_agent = {
 }
 url = f"https://www.google.com/search?sca_esv=450a9a9b983914d8&sxsrf=ACQVn0__adjDE93ghjaoFfM8i9gxgORnQg:1714569588116&q={user}&uds=AMwkrPuOzfHW47FsQBhXV8rx0VyLsuGpbfE881Wx3U5JO9VG7rHfs9Ah6EfWvTERlEnwEuAPNodB7Tu1njaNPt6jHVFYNbmBbM9q7Db4wruU6-bMk3tLizdjTFJIjyYxDVbCZrb14x_shTQ9N4PWhLcdxNk9_TxKy3Zvrth-m1M2ofaAc9eA7wVjg4oZ9U-uJTY0I95R9758AGLunhA97trQSnFuqYf_wgII56GiX4HOjf9hXHvwkxEqqKsmiQO-jsnskP4QKyx5wFvihvqMRFvRCWplwwTCpB-EUcUgXkEQ8jfoeK_U0Uw&udm=2&prmd=invsmbtz&sa=X&ved=2ahUKEwj81_e5xeyFAxWAcGwGHWL_DeEQtKgLegQIDRAB&biw=1163&bih=539&dpr=1.65"
 
-response = requests.get(url = url, headers=user_agent).content
+response = requests.get(url = url, headers=user_agent).text
 
-print(response)
+#print(response)
+
+pattern = "\[\"https://.*\.jpg\",[0-9]+,[0-9]+\]"
+
+images = re.findall(pattern, response)
+
+#print(images)
+for image in images:
+    print(image)
+    #print(type(image))
+    #image_url = eval(image)[0]
+    #print(image_url)
+
+#["https://earthsky.org/upl/2023/01/Moon-Apogee-Perigee-Niveth-Kumar-12-8-2022-800x706.jpg",706,800]
