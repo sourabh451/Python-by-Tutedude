423f1be96ea8270943d90adfe7813453e4f375ca
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-03 14:19:34 +0530

    Module 17 : WEB SCRAPING 18. Downloading images 4

--- 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web_6_b.py
+++ 17WEB_SCRAPING/Retrieving_content_from_webpages_17_3/web_6_b.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import os
 user = input("Enter the image name: ")
 user_agent = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
@@ -12,9 +13,21 @@ print(f"Total Images : {len(images)}")
 no_of_images = int(input("Number of images to be downloaded: "))
 
 if images:
+    if not os.path.exists(user):
+        os.mkdir(user)
+        os.chdir(user)
+
+    else:
+        os.chdir(user)
+
     for image_url in images[:no_of_images]:
         #print(image_url)
         #print(type(image_url))
         #image_url = eval(image)[0]
         response = requests.get(url= image_url).content
-        print(response)
+        #print(response)
+        #print(image_url)
+        image_name = image_url.split('/')[-1]
+
+        with open(image_name,"wb") as file:
+            file.write(response)
