6acab1f9bedda108d8763d14eab99f3460eb32b6
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-04 21:18:47 +0530

    Module 19 : AUTOMATION WITH SELENIUM 1. Installation

---
+++ 19AUTOMATION_WITH_SELENIUM/Selenium_19_1/sel_1.py
@@ -0,0 +1,2 @@
+#  pip install selenium
+#  download firefox webdirver from https://github.com/mozilla/geckodriver/releases as per firefox version and extract in C:\Program Files (x86)\Mozilla Firefox
