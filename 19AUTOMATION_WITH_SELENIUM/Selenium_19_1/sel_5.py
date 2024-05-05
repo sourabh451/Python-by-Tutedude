4019a86c4037d6283d63236c3e60b1f08647d509
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-05 08:33:31 +0530

    Module 19 : AUTOMATION WITH SELENIUM 9. Getting data

--- 19AUTOMATION_WITH_SELENIUM/Selenium_19_1/sel_5.py
+++ 19AUTOMATION_WITH_SELENIUM/Selenium_19_1/sel_5.py
@@ -8,3 +8,13 @@ driver.get('http://www.amazon.in')
 driver.maximize_window()
 driver.find_element(By.XPATH,"//input[@id='twotabsearchtextbox']").send_keys('iphones')
 driver.find_element(By.XPATH,"//input[@id='nav-search-submit-button']").click()
+
+list = driver.find_elements(By.XPATH,"//span[@class='a-size-medium a-color-base a-text-normal']")
+
+# 00 products found
+print(str(len(list)) + 'products found')
+
+for i in list:
+    print(i.text)
+
+driver.quit()
