d873ec9466bd9fc9808327d1d4905959e58b0911
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-06 10:55:22 +0530

    Module 22 : Network Programming In Python Using Sockets : Building A Chat Application 3. Creating A Client

---
+++ 22Network_Programming_In_Python_Using_Sockets_Building_A_Chat_Application/networking_22_1/client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+HOST_NAME = socket.gethostname()
+PORT = 12345
+
+s.connect((HOST_NAME,PORT))
+
+# Run server and then client and check server terminal it will give one ip address for both server and client because we r using the same computer
---
+++ 22Network_Programming_In_Python_Using_Sockets_Building_A_Chat_Application/networking_22_1/server.py
@@ -0,0 +1,16 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # AF_INET AF is address family and INET is socket type on ip address and SOCK_STREAM is socket type on TCP
+
+HOST_NAME = socket.gethostname()
+#IP = '127.0.0.0' as we are same computer we use host name
+#print(HOST_NAME)
+PORT = 12345
+
+s.bind((HOST_NAME,PORT))
+
+s.listen(4) # 4 is called backlog specifies accepted connection that the system allows refusing new connection this is consern with multiply client to server this is used to liten connection from client
+
+while True:
+    client, address = s.accept()
+    print(address)
