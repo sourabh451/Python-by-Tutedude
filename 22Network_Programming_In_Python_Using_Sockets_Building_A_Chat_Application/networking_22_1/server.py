b41fbb24e365e28f1600ce795785308eff066063
Sourabha Patil <er.sourabhapatil@gmail.com>
2024-05-06 13:22:40 +0530

    Module 22 : Network Programming In Python Using Sockets : Building A Chat Application 8. Programming Send Functionality

--- 22Network_Programming_In_Python_Using_Sockets_Building_A_Chat_Application/networking_22_1/server.py
+++ 22Network_Programming_In_Python_Using_Sockets_Building_A_Chat_Application/networking_22_1/server.py
@@ -6,6 +6,12 @@ def send(listbox,entry):
     message = entry.get()
     listbox.insert('end',message)
     entry.delete(0,END)
+    client.send(bytes(message, "utf-8"))
+
+def recieve(listbox):
+    message_from_client = client.recv(50)
+    listbox.insert('end',message_from_client.decode('utf-8'))
+
 
 root = Tk()
 
@@ -18,7 +24,9 @@ listbox.pack()
 button = Button(root,text="Send",command=lambda :send(listbox,entry))
 button.pack(side=BOTTOM)
 
-root.mainloop()
+rbutton = Button(root,text="Recieve",command=lambda :recieve(listbox))
+rbutton.pack(side=BOTTOM)
+#root.mainloop()
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # AF_INET AF is address family and INET is socket type on ip address and SOCK_STREAM is socket type on TCP
 
@@ -31,8 +39,12 @@ s.bind((HOST_NAME,PORT))
 
 s.listen(4) # 4 is called backlog specifies accepted connection that the system allows refusing new connection this is consern with multiply client to server this is used to liten connection from client
 client, address = s.accept()
+'''
 while True:
-    message = input('Server:')
-    client.send(bytes(message,"utf-8")) # we can send message through bytes
+    #message = input('Server:')
+    #client.send(bytes(message,"utf-8")) # we can send message through bytes
     message_from_client = client.recv(50)
     print("Client:"+message_from_client.decode('utf-8'))
+'''
+
+root.mainloop()
